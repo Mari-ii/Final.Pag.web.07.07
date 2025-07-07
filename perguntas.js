49bfea221d598c14bb102d6f540bfc212b36944f
Mari-ii <mascarello.maria@escola.pr.gov.br>
2025-07-07 11:15:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Astronomia',
+    'Qual é o planeta mais distante do Sol no Sistema Solar' ,
+    'Netuno'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é a galáxia que contém o nosso Sistema Solar?',
+    'Via Láctea'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é o planeta mais próximo do Sol?',
+    'Mercúrio'
+)
+
+criaCartao(
+    'Astronomia',
+    'O que é uma Galáxia?',
+    'Uma grande coleção de estrelas, gás e poeira cósmica'
+)
+
+criaCartao(
+    'Astronomia',
+    'O que é um buraco negro?',
+    'Uma região do espaço com gravidade tão forte que nada pode escapar, nem mesmo a luz'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é o processo pelo qual as estrelas geram energia?',
+    'Fusão Nuclear'
+)
+
+criaCartao(
+    'Astronimia',
+    'O que é um asteroide?',
+    'Um objeto rochoso que orbita o sol'
+)
+
+criaCartao(
+    'Astronomia',
+    'O que é uma Supernova?',
+    'É a explosão estelar poderosa e luminosa que ocorre no final da vida de uma estrela massiva'
+)
+
+criaCartao(
+    'Astronomia',
+    ' Qual é o fenômeno que ocorre quando a Lua passa entre a Terra e o Sol?',
+    'Ocorre um Eclipse'
+)
